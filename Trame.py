e60c2603aedd29d7b67653e59f19797de3c7b264
Cynthia <72271024+cynhach@users.noreply.github.com>
2022-11-10 00:25:45 +0100

    Add files via upload

---
+++ Trame.py
@@ -0,0 +1,14 @@
+class Trame:
+	d = 1
+	def __init__(self,trame):
+		self.trame = trame.lower()
+		self.adresseDest = trame[0:12]
+		self.adresseSource = trame[12:24]
+		self.type = trame[24:28]
+		self.data = trame[28:]
+		self.paquet = IPv4(self.data)
+		Trame.id += 1
+
+
+	def toString(self);
+		return "Trame numero {}:\nEthernet:\n\tAdresse MAC Destination: {}\n\tAdresse MAC Source: {}\n\tType (Ox{}): {}".format(self.id, self.destMac, self.srcMac, self.type["Hexadecimal"], self.type["Definition"]) + self.paquet.toString()
