6da04eea80ac8f99658aaa0cba328e5cc1b0020f
Cynthia <72271024+cynhach@users.noreply.github.com>
2022-11-10 00:29:39 +0100

    Update Trame.py

--- Trame.py
+++ Trame.py
@@ -2,9 +2,11 @@ class Trame:
 	d = 1
 	def __init__(self,trame):
 		self.trame = trame.lower()
+		self.entete = trame[0:28]
+		self.id = Trame.id
 		self.adresseDest = trame[0:12]
 		self.adresseSource = trame[12:24]
-		self.type = trame[24:28]
+		self.type = self.entete[24:28]
 		self.data = trame[28:]
 		self.paquet = IPv4(self.data)
 		Trame.id += 1
